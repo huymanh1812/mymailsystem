bef5b7e493de82a5918df2bec90ba09f4588dbb9
Kien Tran <kien868788@gmail.com>
2019-12-23 02:09:06 +0700

    update

--- src/mymailsystem2/Envelope.java
+++ src/mymailsystem2/Envelope.java
@@ -22,8 +22,6 @@ public class Envelope {
     /* Target MX-host */
     public String DestHost;
     public InetAddress DestAddr;
-  
-
 
 
     /* The actual message */
@@ -31,54 +29,54 @@ public class Envelope {
 
     /* Create the envelope. */
     public Envelope(Message message, String localServer) throws UnknownHostException {
-	/* Get sender and recipient. */
-	Sender = message.getFrom();
-	Recipient = message.getTo();
+        /* Get sender and recipient. */
+        Sender = message.getFrom();
+        Recipient = message.getTo();
 
 	/* Get message. We must escape the message to make sure that
 	   there are no single periods on a line. This would mess up
 	   sending the mail. */
-	Message = escapeMessage(message);
+        Message = escapeMessage(message);
 
-	/* Take the name of the local mailserver and map it into an
-	 * InetAddress */
-	DestHost = localServer;
-	try {
-	    DestAddr = InetAddress.getByName(DestHost);
-	} catch (UnknownHostException e) {
-	    System.out.println("Unknown host: " + DestHost);
-	    System.out.println(e);
-	    throw e;
-	}
-	return;
+        /* Take the name of the local mailserver and map it into an
+         * InetAddress */
+        DestHost = localServer;
+        try {
+            DestAddr = InetAddress.getByName(DestHost);
+        } catch (UnknownHostException e) {
+            System.out.println("Unknown host: " + DestHost);
+            System.out.println(e);
+            throw e;
+        }
+        return;
     }
 
     /* Escape the message by doubling all periods at the beginning of
        a line. */
     private Message escapeMessage(Message message) {
-	String escapedBody = "";
-	String token;
-	StringTokenizer parser = new StringTokenizer(message.Body, "\n", true);
+        String escapedBody = "";
+        String token;
+        StringTokenizer parser = new StringTokenizer(message.Body, "\n", true);
 
-	while(parser.hasMoreTokens()) {
-	    token = parser.nextToken();
-	    if(token.startsWith(".")) {
-		token = "." + token;
-	    }
-	    escapedBody += token;
-	}
-	message.Body = escapedBody;
-	return message;
+        while (parser.hasMoreTokens()) {
+            token = parser.nextToken();
+            if (token.startsWith(".")) {
+                token = "." + token;
+            }
+            escapedBody += token;
+        }
+        message.Body = escapedBody;
+        return message;
     }
 
     /* For printing the envelope. Only for debug. */
     public String toString() {
-	String res = "Sender: " + Sender + '\n';
-	res += "Recipient: " + Recipient + '\n';
-	res += "MX-host: " + DestHost + ", address: " + DestAddr + '\n';
-	res += "Message:" + '\n';
-	res += Message.toString();
-	
-	return res;
+        String res = "Sender: " + Sender + '\n';
+        res += "Recipient: " + Recipient + '\n';
+        res += "MX-host: " + DestHost + ", address: " + DestAddr + '\n';
+        res += "Message:" + '\n';
+        res += Message.toString();
+
+        return res;
     }
 }
--- src/mymailsystem2/SMTPConnection.java
+++ src/mymailsystem2/SMTPConnection.java
@@ -22,89 +22,89 @@ public class SMTPConnection {
     /* Create an SMTPConnection object. Create the socket and the
        associated streams. Initialize SMTP connection. */
     public SMTPConnection(Envelope envelope) throws IOException {
- 
-    	connection = new Socket("localhost",SMTP_PORT);
-    	fromServer = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-    	toServer = new DataOutputStream(connection.getOutputStream());
- 
-	 /* Fill in */
+
+        connection = new Socket("localhost", 25000);
+        fromServer = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        toServer = new DataOutputStream(connection.getOutputStream());
+
+        /* Fill in */
 	 /* Read a line from server and check that the reply code is 220.
 	    If not, throw an IOException. */
-	 String text = fromServer.readLine();
-	 System.out.println(parseReply(text));
-	 if (parseReply(text) != 220)
-	 throw new IOException("Reply code not 220");
-	 System.out.println("Reply code not 220");
-	 
+        String text = fromServer.readLine();
+        System.out.println(parseReply(text));
+        if (parseReply(text) != 220)
+            throw new IOException("Reply code not 220");
+
 	 /* SMTP handshake. We need the name of the local machine.
 	    Send the appropriate SMTP handshake command. */
-	 InetAddress localhost = InetAddress.getByName("localhost");
-	 System.out.println("LOCALHOST: "+localhost);
-	 sendCommand( "HELO " + localhost + CRLF, 250 );
-	 isConnected = true;
-	    }
-	
-	    /* Send the message. Write the correct SMTP-commands in the
-	       correct order. No checking for errors, just throw them to the
-	       caller. */
-	    public void send(Envelope envelope) throws IOException {
+        InetAddress localhost = InetAddress.getByName("localhost");
+        System.out.println("LOCALHOST: " + localhost);
+        sendCommand("HELO " + localhost, 250);
+        isConnected = true;
+    }
+
+    /* Send the message. Write the correct SMTP-commands in the
+       correct order. No checking for errors, just throw them to the
+       caller. */
+    public void send(Envelope envelope) throws IOException {
 	 /* Send all the necessary commands to send a message. Call
 	    sendCommand() to do the dirty work. Do _not_ catch the
 	    exception thrown from sendCommand(). */
-	 /* Fill in */
-	     sendCommand("MAIL FROM: " + envelope.Sender + CRLF,250);
-	     sendCommand("ReCIPIENT TO: " + envelope.Recipient + CRLF ,250);
-	     sendCommand("DATA"+ CRLF ,354);
-	 }
-	
-	    /* Close the connection. First, terminate on SMTP level, then
-	       close the socket. */
-	    public void close() {
-	 isConnected = false;
-	 try {
-	 sendCommand("QUIT" + CRLF, 221);
-	 connection.close();
-	 }
-	 catch (IOException e) {
-	 System.out.println("The system can not close connection: " + e);
-	 isConnected = true;
-	 }
-	    }
-	
-	    /* Send an SMTP command to the server. Check that the reply code is
-	       what is is supposed to be according to RFC 821. */
-	    private void sendCommand(String command, int rc) throws IOException {
-	 
-	 /* Write command to server and read reply from server. */
-	    System.out.println("Command to server: " + command +CRLF);
-	    toServer.writeBytes(command+CRLF);
-	  
+        /* Fill in */
+        sendCommand("MAIL FROM: " + envelope.Sender, 250);
+        sendCommand("RCPT TO: " + envelope.Recipient, 250);
+        sendCommand("DATA", 354);
+        sendCommand(envelope.Message.toString(),250);
+    }
+
+    /* Close the connection. First, terminate on SMTP level, then
+       close the socket. */
+    public void close() {
+        isConnected = false;
+        try {
+            sendCommand("QUIT", 221);
+            connection.close();
+        } catch (IOException e) {
+            System.out.println("The system can not close connection: " + e);
+            isConnected = true;
+        }
+    }
+
+    /* Send an SMTP command to the server. Check that the reply code is
+       what is is supposed to be according to RFC 821. */
+    private void sendCommand(String command, int rc) throws IOException {
+
+        /* Write command to server and read reply from server. */
+        System.out.println("Command to server: " + command);
+        toServer.writeBytes(command.concat(CRLF));
+
 	 /* Check that the server's reply code is the same as the parameter
 	    rc. If not, throw an IOException. */
-	     String text = fromServer.readLine();
-	     System.out.println("Server reply: " + text);
-	     
-	     if (parseReply(text) != rc){
-	     System.out.println("The reply code is not the same as the rc");
-	     throw new IOException("The reply code is not the same as the rc");
-	     }
-	    }
-	
-	    /* Parse the reply line from the server. Returns the reply code. */
-	    private int parseReply(String reply) {
-	 
-	     StringTokenizer tokens = new StringTokenizer(reply," ");
-	     String rc = tokens.nextToken();
-	     int x = Integer.parseInt(rc);
-	
-	     return x; 
-	     
-	    }
-	    /* Destructor. Closes the connection if something bad happens. */
-	    protected void finalize() throws Throwable {
-	 if(isConnected) {
-	     close();
-	 }
-	 super.finalize();
-	 }
+        String text = fromServer.readLine();
+        System.out.println("Server reply: " + text);
+
+        if (parseReply(text) != rc) {
+            System.out.println("The reply code is not the same as the rc");
+            throw new IOException("The reply code is not the same as the rc");
+        }
+    }
+
+    /* Parse the reply line from the server. Returns the reply code. */
+    private int parseReply(String reply) {
+
+        StringTokenizer tokens = new StringTokenizer(reply, " ");
+        String rc = tokens.nextToken();
+        int x = Integer.parseInt(rc);
+
+        return x;
+
+    }
+
+    /* Destructor. Closes the connection if something bad happens. */
+    protected void finalize() throws Throwable {
+        if (isConnected) {
+            close();
+        }
+        super.finalize();
+    }
 }
